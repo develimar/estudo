ff3902e483b3c55cd186e40e95b4b1769935a4d5
elimar.pc@gmail.com <Eli425136mar>
2016-11-19 21:05:15 -0200

    Foi Criado o pacote de idiomas pt-br Configurado o pacote de idiomas dentro de app.php juntamente com nome da aplicação criado a factory model com os dados que seram inseridos no banco foram criados as seguintes migrations produtcts - clients foram criados as models Client.php - Product.php -

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -16,11 +17,20 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username', 30)->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
+
+        factory(User::class)->create([
+            'name'=>'Elimar Santos',
+            'username'=>'admin',
+            'email'=>'elimar.pc@gmail.com',
+            'password'=>bcrypt('admin'),
+        ]);
     }
 
     /**
